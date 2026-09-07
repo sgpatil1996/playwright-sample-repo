e230f325835a3080d2ee5c01fb59fb23c45e581f
Shruti Patil <sgpatil1996@gmail.com>
2026-09-07 17:55:43 +0530

    credential changes

--- pages/LoginPage.ts
+++ pages/LoginPage.ts
@@ -22,6 +22,6 @@ export class LoginPage {
   async login(username: string, password: string) {
     await this.usernameInput.fill(username);
     await this.passwordInput.fill(password);
-    await this.loginButton.click()
+    await this.loginButton.click();
   }
 }
