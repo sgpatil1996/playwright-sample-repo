ebf106851e76d0b1fff2307ad7e1fa0581a79c6a
Shruti Patil <sgpatil1996@gmail.com>
2026-09-07 19:40:30 +0530

    added stage

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,7 +2,9 @@ import { defineConfig, devices } from '@playwright/test';
 import * as dotenv from 'dotenv';
 import * as path from 'path';
 
-const environment = process.env.TEST_ENV || 'qa';    //Declare the variable
+//const environment = process.env.TEST_ENV || 'qa';    //Declare the variable
+const environment = process.env.TEST_ENV || 'staging';    //Declare the variable
+
 dotenv.config({ path: path.resolve(__dirname, `.env.${environment}`) });  //Pass it into the configuration resolver
 
 export default defineConfig({
